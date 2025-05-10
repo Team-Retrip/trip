9ba1630b26be31c0673fad13b63656dc9db75f63
TueBack <p_caso@naver.com>
2025-05-10 12:53:02 +0900

    deploy test2

--- src/main/java/com/retrip/trip/infra/adapter/in/presentation/rest/TestController.java
+++ src/main/java/com/retrip/trip/infra/adapter/in/presentation/rest/TestController.java
@@ -33,7 +33,7 @@ public class TestController {
         responseData.put("port",port);
         responseData.put("adress:",address);
         responseData.put("name","이혁진");
-        responseData.put("age","30");
+        responseData.put("age","28");
         responseData.put("test","test");
 
         return ResponseEntity.ok(responseData);
