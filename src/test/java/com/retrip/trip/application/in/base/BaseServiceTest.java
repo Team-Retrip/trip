7fa3f2cb3ca86e515ec95f833e6dc12972292781
mandykr <hongseokii92@gmail.com>
2025-05-29 09:30:56 +0900

    refactor: BaseServiceTest 추가

---
+++ src/test/java/com/retrip/trip/application/in/base/BaseServiceTest.java
@@ -0,0 +1,12 @@
+package com.retrip.trip.application.in.base;
+
+import com.retrip.trip.infra.config.QuerydslConfig;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@DataJpaTest
+@Import(QuerydslConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public abstract class BaseServiceTest {
+}
---
+++ src/test/java/com/retrip/trip/application/in/base/BaseTripInvitationServiceTest.java
@@ -0,0 +1,22 @@
+package com.retrip.trip.application.in.base;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.retrip.trip.application.in.TripInvitationService;
+import com.retrip.trip.application.out.repository.TripRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class BaseTripInvitationServiceTest extends BaseServiceTest {
+    @Autowired
+    protected TripRepository tripRepository;
+
+    @Autowired
+    protected JPAQueryFactory jpaQueryFactory;
+
+    protected TripInvitationService tripInvitationService;
+
+    @BeforeEach
+    void setUp() {
+        tripInvitationService = new TripInvitationService(tripRepository);
+    }
+}
