ea09706ccb36f14bafc54b3c0a9d79317b15d18d
mandykr <hongseokii92@gmail.com>
2025-06-04 08:52:19 +0900

    chore: 여행 초대 생성 API uri 수정

--- src/main/java/com/retrip/trip/application/in/usecase/TripInvitationManageUseCase.java
+++ src/main/java/com/retrip/trip/application/in/usecase/TripInvitationManageUseCase.java
@@ -3,6 +3,8 @@ package com.retrip.trip.application.in.usecase;
 import com.retrip.trip.application.in.request.TripInvitationsCreateRequest;
 import com.retrip.trip.application.in.response.TripInvitationsCreateResponse;
 
+import java.util.UUID;
+
 public interface TripInvitationManageUseCase {
-    TripInvitationsCreateResponse createInvitations(TripInvitationsCreateRequest request);
+    TripInvitationsCreateResponse createInvitations(UUID tripId, TripInvitationsCreateRequest request);
 }
